c817a06592b0c00be5dc504889e6b399a341d83c
TanvirMobasshir <mdtanvirmobasshir@gmail.com>
2022-02-27 20:35:50 +0600

    problem 1-7 done

--- LAB/lab_2/4.py
+++ LAB/lab_2/4.py
@@ -1,8 +1,7 @@
-# string = "Practice Problems to Drill List Comprehension in Your Head."
-# vowels = ['a', 'e', 'i', 'o', 'u']
-# new_string = ''
-#
-# for i in string.lower():
-#     if i not in vowels: new_string = new_string + i
-#
-# print(new_string)
+string = "Practice Problems to Drill List Comprehension in Your Head."
+vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+
+for vowel in vowels:
+    string = string.replace(vowel, '')
+
+print(string)
