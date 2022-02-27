aed2128c42a290c64bea636e244e7039f9dbcd31
TanvirMobasshir <mdtanvirmobasshir@gmail.com>
2022-02-27 19:39:55 +0600

    NOT SOLVED YET

--- LAB/lab_2/4.py
+++ LAB/lab_2/4.py
@@ -1,8 +1,8 @@
-string = "Practice Problems to Drill List Comprehension in Your Head."
-vowels = ['a', 'e', 'i', 'o', 'u']
-new_string = ''
-
-for i in string.lower():
-    if i not in vowels: new_string = new_string + i
-
-print(new_string)
+# string = "Practice Problems to Drill List Comprehension in Your Head."
+# vowels = ['a', 'e', 'i', 'o', 'u']
+# new_string = ''
+#
+# for i in string.lower():
+#     if i not in vowels: new_string = new_string + i
+#
+# print(new_string)
--- LAB/lab_2/7.py
+++ LAB/lab_2/7.py
@@ -1,3 +1,3 @@
-nums = [i for i in range(1, 1001)]
-numbers = [i for i in nums for j in range(2, 10) if i % j == 0]
-print(numbers)
+# nums = [i for i in range(1, 1001)]
+# numbers = [i for i in nums for j in range(2, 10) if i % j == 0]
+# print(numbers)
