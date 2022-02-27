c0827a97de35032c2726ce1165d77928450f3aaf
TanvirMobasshir <mdtanvirmobasshir@gmail.com>
2022-02-27 19:39:00 +0600

    problem 4 and 7 not solved yet

--- LAB/lab_2/7.py
+++ LAB/lab_2/7.py
@@ -1,3 +1,3 @@
 nums = [i for i in range(1, 1001)]
-numbers = list(set([i for i in nums for j in range(2, 10) if i % j == 0]))
+numbers = [i for i in nums for j in range(2, 10) if i % j == 0]
 print(numbers)
