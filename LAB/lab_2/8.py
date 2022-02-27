4623e299ba7fe1966d16683bb32a2e4964a1e20b
TanvirMobasshir <mdtanvirmobasshir@gmail.com>
2022-02-27 17:14:38 +0600

    added solution to problem 8 of lab2

--- LAB/lab_2/8.py
+++ LAB/lab_2/8.py
@@ -1,2 +1,11 @@
 nums = [i for i in range(1, 1001)]
-numbers = []
+divisors = [i for i in range(1, 10)]
+highest_divisor = {}
+
+for i in nums:
+    for j in reversed(divisors):
+        if i % j == 0:
+            highest_divisor[i] = j
+            break
+
+print(highest_divisor)
