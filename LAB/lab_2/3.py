c518e7b3d16367140e042dcc554c17503e686117
TanvirMobasshir <mdtanvirmobasshir@gmail.com>
2022-02-27 19:24:08 +0600

    used string method for problem 3, 4

--- LAB/lab_2/3.py
+++ LAB/lab_2/3.py
@@ -1,7 +1,2 @@
 string = "Practice Problems to Drill List Comprehension in Your Head."
-count = 0
-
-for i in string:
-    if i == ' ': count += 1
-
-print(f"Number of spaces in the string is {count}")
+print(f"Number of spaces in the string is {string.count(' ')}")
