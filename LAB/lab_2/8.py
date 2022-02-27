ff0d8ab65debf545cb8a892806beb41412d6c6c6
TanvirMobasshir <mdtanvirmobasshir@gmail.com>
2022-02-27 19:08:01 +0600

    converted the solution of 8th problem to comprehension

--- LAB/lab_2/8.py
+++ LAB/lab_2/8.py
@@ -1,10 +1,4 @@
 nums = [i for i in range(1, 1001)]
 divisors = [i for i in range(1, 10)]
-highest_divisors = {number: divisor for number in nums for divisor in reversed(divisors) if number % divisor == 0}
-
-# highest_divisors = {}
-# for i in nums:
-#     for j in reversed(divisors):
-#         if i % j == 0:
-#             highest_divisors[i] = j
+highest_divisors = {number: divisor for number in nums for divisor in divisors if number % divisor == 0}
 print(highest_divisors)
